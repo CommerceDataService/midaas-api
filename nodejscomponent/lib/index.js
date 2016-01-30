44f1212b0332337f972cede8faf792904bb80c45
Michael Balint <michael.balint@pif.gov>
2016-01-30 14:40:21 -0500

    Added `getIncomeMean` lambda function and associated endpoint.

--- nodejscomponent/lib/index.js
+++ nodejscomponent/lib/index.js
@@ -2,11 +2,34 @@
  * Lib
  */
 
-module.exports.respond = function(event, cb) {
+ var _       = require("lodash");
+ var mysql   = require("mysql");
+ var numeral = require("numeraljs");
 
-  var response = {
-    message: "Your Serverless function ran successfully!"
-  };
+ var conn_options = {};
+ var connection = mysql.createConnection(conn_options);
 
-  return cb(null, response);
-};
+module.exports.getIncomeMean = function(event, callback) {
+
+  var sql = "SELECT AVG(WAGP)*ADJINC/1000000 AS MEAN_INCOME FROM PUMS_2014_Persons";
+
+  validQueries = _.pick(event, ["PUMA", "SEX", "RAC1P"]);
+  // console.log(validQueries);
+  if(!_.isEmpty(validQueries)) {
+    sql += " WHERE ";
+    sqlWhere = [];
+    _.forOwn(validQueries, function(value, key) {
+      sqlWhere.push(key + "=" + value);
+    });
+    sql += sqlWhere.join(" && ");
+  }
+  sql += ";";
+  // console.log(sql);
+
+  connection.query(sql, function(err, results, fields){
+    var mean = results[0]["MEAN_INCOME"];
+    var formattedMean = numeral(mean).format("($0.00a)");
+    return callback(err, {"mean": formattedMean});
+	});
+
+}
