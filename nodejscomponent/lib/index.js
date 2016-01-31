ca3f828ae857228177b183ddab8fa4a43e9e22ae
Michael Balint <michael.balint@pif.gov>
2016-01-31 01:20:14 -0500

    Added query params to lambda functions.

--- nodejscomponent/lib/index.js
+++ nodejscomponent/lib/index.js
@@ -9,21 +9,25 @@ var numeral = require("numeraljs");
 var conn_options = require("./rds-config.json");
 var connection = mysql.createConnection(conn_options);
 
-module.exports.getIncomeMean = function(event, callback) {
-
-  var sql = "SELECT AVG(WAGP)*ADJINC/1000000 AS MEAN_INCOME FROM PUMS_2014_Persons";
-
-  validQueries = _.pick(event, ["PUMA", "SEX", "RAC1P"]);
+var appendWhereClause = function(sql, validQueries) {
   // console.log(validQueries);
   if(!_.isEmpty(validQueries)) {
     sql += " WHERE ";
     sqlWhere = [];
     _.forOwn(validQueries, function(value, key) {
-      sqlWhere.push(key + "=" + value);
+      sqlWhere.push(key + "='" + value +"'");
     });
-    sql += sqlWhere.join(" && ");
+    sql += sqlWhere.join(" AND ");
   }
-  sql += ";";
+  return sql += ";";
+}
+
+module.exports.getIncomeMean = function(event, callback) {
+
+  var sql = "SELECT AVG(WAGP)*ADJINC/1000000 AS MEAN_INCOME FROM PUMS_2014_Persons";
+
+  validQueries = _.pick(event, ["PUMA", "SEX", "RAC1P"]);
+  sql = appendWhereClause(sql, validQueries);
   // console.log(sql);
 
   connection.query(sql, function(err, results, fields){
@@ -38,8 +42,16 @@ module.exports.getIncomeQuantiles = function(event, callback) {
 
   var sql = "SELECT QUANTILE, INCOME FROM PUMS_2014_Quantiles";
 
+  validQueries = _.pick(event, ["state", "race", "sex", "agegroup"])
+  sql = appendWhereClause(sql, validQueries);
+
   connection.query(sql, function(err, results, fields){
-    return callback(err, results);
+    resultsObj = {};
+    _.forEach(results, function(result) {
+      resultsObj[result["QUANTILE"]] = result["INCOME"]
+    })
+
+    return callback(err, resultsObj);
 	});
 
 }
