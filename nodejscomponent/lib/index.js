ba42ef96b858531635f4923322fa2a724f0c92f5
Michael Balint <michael.balint@pif.gov>
2016-02-10 21:58:10 -0500

    Converted functions to use postgres.

--- nodejscomponent/lib/index.js
+++ nodejscomponent/lib/index.js
@@ -3,39 +3,48 @@
  */
 
 var _       = require("lodash");
-var mysql   = require("mysql");
+var pg      = require("pg");
 var numeral = require("numeraljs");
 
 var utils   = require("./utils.js");
 
-var conn_options = require("./rds-config.json");
-var connection = mysql.createConnection(conn_options);
+var conn_options = require("./redshift-config.json");
 
 module.exports.getIncomeQuantiles = function(event, callback) {
 
-  queryParams = _.pick(event, ["state", "race", "sex", "agegroup"]);
+  var queryParams = _.pick(event, ["state", "race", "sex", "agegroup"]);
 
   utils.validateQueryParams(queryParams, function(err, validateCallback) {
     if(err) { return callback(err); }
 
     var sql = "SELECT QUANTILE, INCOME FROM PUMS_2014_Quantiles";
     sql = utils.appendWhereClause(sql, queryParams) + ";";
+    // return callback(null, sql);
 
-    connection.query(sql, function(err, results, fields){
-      resultsObj = {};
-      _.forEach(results, function(result) {
-        resultsObj[result["QUANTILE"]] = result["INCOME"];
-      })
+    var client = new pg.Client(conn_options);
+    client.connect(function(err) {
+      if(err) { return callback(err); }
 
-      return callback(err, resultsObj);
-  	});
+      client.query(sql, function(err, response) {
+        client.end();
+        if(err) { return callback(err); }
+
+        var results = response.rows;
+        resultsObj = {};
+        _.forEach(results, function(result) {
+          resultsObj[result["quantile"]] = result["income"];
+        })
+
+        return callback(err, resultsObj);
+      });
+    });
   });
 
 }
 
 module.exports.getIncomeDistribution = function(event, callback) {
 
-  queryParams = _.pick(event, ["state", "race", "sex", "agegroup"]);
+  var queryParams = _.pick(event, ["state", "race", "sex", "agegroup"]);
 
   utils.validateQueryParams(queryParams, function(err, validateCallback) {
     if(err) { return callback(err); }
@@ -48,21 +57,31 @@ module.exports.getIncomeDistribution = function(event, callback) {
     sql = utils.appendTranslatedWhereClause(sql, queryParams);
     sql += " GROUP BY BUCKET;";
     // return callback(null, sql);
-    connection.query(sql, function(err, results, fields){
-      resultsObj = {};
-      total = _.sumBy(results, (function(result) {
-        return result["COUNT"];
-      }));
-      _.forEach(results, function(result, i) {
-        start = utils.formatIncome(results[i]["BUCKET"]);
-        end = utils.formatIncome(results[i]["BUCKET"] + 10000);
-        key = "" + start + "-" + end;
-        // key = result["BUCKET"];
-        resultsObj[key] = result["COUNT"]/total;
-      })
 
-      return callback(err, resultsObj);
-  	});
+    var client = new pg.Client(conn_options);
+    client.connect(function(err) {
+      if(err) { return callback(err); }
+
+      client.query(sql, function(err, response) {
+        client.end();
+        if(err) { return callback(err); }
+
+        var results = response.rows;
+        resultsObj = {};
+        total = _.sumBy(results, (function(result) {
+          return result["count"];
+        }));
+        _.forEach(results, function(result, i) {
+          start = utils.formatIncome(results[i]["bucket"]);
+          end = utils.formatIncome(results[i]["bucket"] + 10000);
+          key = "" + start + "-" + end;
+          // key = result["bucket"];
+          resultsObj[key] = result["count"]/total;
+        })
+
+        return callback(err, resultsObj);
+      });
+    });
   });
 
 }
